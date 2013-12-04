e53aa31f45390f891699cfc3440891b63cbe3fc5
railla <dev.local@gmail.com>
2013-12-04 11:18:39 +0100

    catch SIGINT to close connection to MPD; check for 30sec playback

--- bin/amfm.pl
+++ bin/amfm.pl
@@ -12,26 +12,36 @@ $| = 1;
 my %SETTINGS = (
     username => $ENV{LASTFM_USERNAME},
     password => $ENV{LASTFM_PASSWORD},
-    mpd_host => "localhost",
-    mpd_port => "6600",
+    mpd_host => $ENV{MPD_HOST} || "localhost",
+    mpd_port => $ENV{MPD_PORT} || "6600",
     autosubmit => 0,
     date_format => "%a %b %e %H:%M:%S %Y",
+);
+
+my $TICK = 5;
+my $MIN_PLAY_TIME = 30;
+my $API_KEY = "7c04baa41513c100f7544a329ac97638";
+my $SECRET = "c1e017252469c6387459e6e7b51d6f53";
+my $URL_ROOT = "https://ws.audioscrobbler.com/2.0/";
+
+my %STATE = (
+    artist => undef,
+    track => undef,
     token => undef,
     session_key => undef,
     mpd_socket => undef,
+    updated => undef,
+    scrobbled => 0,
+    running => 1,
 );
 
-my $min_play_time = 30;
-my $API_KEY = "7c04baa41513c100f7544a329ac97638";
-my $secret = "c1e017252469c6387459e6e7b51d6f53";
-my $URL_ROOT = "https://ws.audioscrobbler.com/2.0/";
 my $curl = WWW::Curl::Easy->new();
 
 sub sign_url {
     my %params = @_;
     $params{api_key} = $API_KEY;
     $params{format} = 'json';
-    my $sign = (join '', map {(!($_ eq 'format' or $_ eq 'callback')) ? $_.$params{$_} : ''} sort keys %params).$secret;
+    my $sign = (join '', map {(!($_ eq 'format' or $_ eq 'callback')) ? $_.$params{$_} : ''} sort keys %params).$SECRET;
     my $res = join('&', (map {$_."=".$params{$_}} keys %params), "api_sig=".md5_hex($sign));
     return $res;
 };
@@ -44,13 +54,17 @@ sub mpd_connect {
     ) or die "Could not create socket: $!\n";
 
     my $data = <$socket>;
-    $SETTINGS{mpd_socket} = $socket;
+    if (!$data) {
+        warn "Cannot connect to MPD";
+        return;
+    }
+    $STATE{mpd_socket} = $socket;
     return $socket;
 }
 
 sub mpd_command {
     my $cmd = shift;
-    my $socket = $SETTINGS{mpd_socket};
+    my $socket = $STATE{mpd_socket};
     $socket->send("$cmd\r\n");
     my $res = do { local $/ = "OK\n"; <$socket>};
     $res =~ s/\nOK\n//g;
@@ -75,7 +89,7 @@ sub make_request {
         warn $curl->strerror($retcode);
         return '';
     }
-    return from_json($response);
+    return (defined($response)) ? from_json($response) : {token => undef};
 }
 
 sub handshake {
@@ -84,6 +98,7 @@ sub handshake {
     if (!$auth_token) {
         die 'Cannot get token';
     }
+    $STATE{token} = $auth_token;
     my $req = sign_url(username => $SETTINGS{username},
                        password => $SETTINGS{password},
                        method => "auth.getMobileSession",
@@ -92,13 +107,8 @@ sub handshake {
     warn "[INFO]\tConnecting to Last.fm...";
 
     my $session_key = make_request($URL_ROOT, 1, $req)->{session}->{key};
-    $SETTINGS{session_key} = $session_key;
+    $STATE{session_key} = $session_key;
     return $session_key;
-}
-
-while (!$SETTINGS{session_key}) {
-    handshake();
-    sleep 1;
 }
 
 sub mpd_is_playing {
@@ -106,20 +116,69 @@ sub mpd_is_playing {
     return ($status{state} =~ /play/) ? 1 : 0;
 }
 
-sub update_now_playing {
-    if (mpd_is_playing) {
-        my $song = mpd_command('currentsong');
-        if ($song =~ /Title:\s+(.+)$/m) {
-            my ($artist, $track) = split(/\s+-\s+/, $1);
-            #my ($artist, $track) = ($1, $2);
-            my $req = sign_url(method => 'track.updateNowPlaying',
-                               sk => $SETTINGS{session_key},
-                               artist => $artist, track => $track);
-            warn "[INFO]\tUpdating now playing..";
-            my $session_key = make_request($URL_ROOT, 1, $req)->{session}->{key};
+sub update_current_song {
+    my $song = mpd_command('currentsong');
+    if ($song =~ /Title:\s+(.+)$/m) {
+        warn "[INFO]\t Playing $1";
+        my $old_song = $STATE{title};
+        $STATE{title} = $1;
+        ($STATE{artist}, $STATE{track}) = split(/\s+-\s+/, $1);
+        if (!defined($old_song) or !($1 eq $old_song)) {
+            $STATE{updated} = time;
+            $STATE{scrobbled} = 0;
+            update_now_playing();
         }
     }
 }
-my $mpd_socket = mpd_connect();
-update_now_playing();
-$mpd_socket->close();
+
+sub scrobble {
+    if ($STATE{artist} and $STATE{track}) {
+        warn "[INFO]\tScrobbling track $STATE{artist} - $STATE{track}";
+        my $req = sign_url(method => 'track.scrobble',
+                           timestamp => time,
+                           sk => $STATE{session_key},
+                           artist => $STATE{artist}, track => $STATE{track});
+        say $req;
+        make_request($URL_ROOT, 1, $req)->{session}->{key}; 
+    }
+    $STATE{scrobbled} = 1;
+}
+
+sub update_now_playing {
+    if ($STATE{artist} and $STATE{track}) {
+        my $req = sign_url(method => 'track.updateNowPlaying',
+                           sk => $STATE{session_key},
+                           artist => $STATE{artist}, track => $STATE{track});
+        warn "[INFO]\tUpdating now playing $STATE{artist} - $STATE{track}";
+        make_request($URL_ROOT, 1, $req)->{session}->{key};
+    }
+}
+
+sub run {
+    handshake unless $STATE{session_key};
+    mpd_connect unless $STATE{mpd_socket};
+    return unless mpd_is_playing();
+
+    my $old_title = $STATE{title};
+    my $last_updated = $STATE{updated};
+    my ($artist, $track) = update_current_song;
+
+    if ($last_updated and $STATE{title}) {
+        say time - $STATE{updated};
+    }
+    return if !$last_updated or !$STATE{title} or $STATE{scrobbled}
+              or ((time - $STATE{updated}) < $MIN_PLAY_TIME);
+
+    scrobble;
+}
+
+$SIG{'INT'} = sub {
+        warn "Closing connection to MPD";
+        $STATE{running} = 0;
+        $STATE{mpd_socket}->close() if $STATE{mpd_socket};
+    };
+
+while ($STATE{running}) {
+    run();
+    sleep $TICK;
+}
